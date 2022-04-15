1b33fa1ac07eb559dced0721031734376e527911
Surya S <101696907+s-u-r-y-a-s@users.noreply.github.com>
2022-04-15 11:58:19 +0530

    Update demo.sql

--- demo.sql
+++ demo.sql
@@ -1,17 +1,5 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.6.21 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- Dumping database structure for demo
-CREATE DATABASE IF NOT EXISTS `demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `demo`;
 USE `demo`;
 
 
@@ -29,10 +17,5 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Dumping data for table demo.users: ~1 rows (approximately)
 DELETE FROM `users`;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `password`, `created`) VALUES
 	(5, 'john', 'doe', 'johndoe@example.com', '+00 0000 0000', '$2y$04$jFPJT4gGp44h1YJed8X/4.cfI4194g1Ce8oFBsd16pE7QS.QijLYK', '2017-10-11 00:35:38');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
